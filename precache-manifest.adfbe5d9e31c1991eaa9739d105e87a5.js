1b940b5080accc188a39a690e9c82ba07cb576c6
smfils1 <smfils1@gmail.com>
2020-08-09 17:24:00 +0000

    Updates

---
+++ precache-manifest.adfbe5d9e31c1991eaa9739d105e87a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb23ca5dda8368ea617c90f9afdc411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8ec954a07a37d1335b",
+    "url": "/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "fb1b6f4925cddf5b2ebd",
+    "url": "/static/css/main.8a352121.chunk.css"
+  },
+  {
+    "revision": "bc8ec954a07a37d1335b",
+    "url": "/static/js/2.98d6910c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.98d6910c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1b6f4925cddf5b2ebd",
+    "url": "/static/js/main.e2595e5f.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
